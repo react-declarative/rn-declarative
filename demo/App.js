400e2a926fe4ad34240d0d3f38c9cf79e51ea3b8
Petr Tripolsky <tripolskypetr@gmail.com>
2024-07-31 00:01:25 +0500

    build-patch

--- demo/App.js
+++ demo/App.js
@@ -11,8 +11,9 @@
  */
 
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import {One, FieldType, sleep} from 'rn-declarative';
+import { ScrollView, StyleSheet } from 'react-native';
+import { One, FieldType, sleep } from 'rn-declarative';
+import { OneSlotFactory } from 'rn-declarative-eva';
 
 import {
   ApplicationProvider,
@@ -22,7 +23,7 @@ import {
   Layout,
   Text,
 } from '@ui-kitten/components';
-import {EvaIconsPack} from '@ui-kitten/eva-icons';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import * as eva from '@eva-design/eva';
 
 /**
@@ -542,35 +543,11 @@ export default () => (
   <>
     <IconRegistry icons={EvaIconsPack} />
     <ApplicationProvider {...eva} theme={eva.light}>
-      <Layout style={styles.container}>
-        <Text style={styles.text} category="h1">
-          Welcome to UI Kitten 😻
-        </Text>
-        <Text style={styles.text} category="s1">
-          Start with editing App.js to configure your App
-        </Text>
-        <Text style={styles.text} appearance="hint">
-          For example, try changing theme to Dark by using eva.dark
-        </Text>
-        <Button style={styles.likeButton} accessoryLeft={HeartIcon}>
-          LIKE111x
-        </Button>
-        <One fields={fields} />
-      </Layout>
+      <ScrollView>
+        <OneSlotFactory>
+          <One fields={fields} />
+        </OneSlotFactory>
+      </ScrollView>
     </ApplicationProvider>
   </>
 );
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    textAlign: 'center',
-  },
-  likeButton: {
-    marginVertical: 16,
-  },
-});
