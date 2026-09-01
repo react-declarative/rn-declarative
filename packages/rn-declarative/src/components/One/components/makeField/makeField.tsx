f288ca09e49104ca86b5d50fa8b0725ecabdefd5
Petr Tripolsky <tripolskypetr@gmail.com>
2026-09-01 20:52:53 +0500

    patch

--- packages/rn-declarative/src/components/One/components/makeField/makeField.tsx
+++ packages/rn-declarative/src/components/One/components/makeField/makeField.tsx
@@ -447,7 +447,7 @@ export function makeField(
                 if (!memory.inputUpdate && !memory.objectUpdate) {
                     break;
                 }
-                sleep(APPLY_DELAY);
+                await sleep(APPLY_DELAY);
             }
             memory.inputUpdate = false;
             memory.objectUpdate = false;
